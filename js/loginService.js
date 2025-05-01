ad59144b4563c286c424d6cafcf19cabbaf7020d
Jerson garcia <jersonjavier@gmail.com>
2025-04-30 20:25:11 -0500

    implementacion de dashboard

--- js/loginService.js
+++ js/loginService.js
@@ -9,25 +9,42 @@ document.getElementById("formLogin").addEventListener('submit', function(e){
 function login(email, password){
     let message = ''
     let alertType = ''
-    fetch("htpps://reqres.in/api/login", {
+    localStorage.removeItem('token')
+    fetch("https://reqres.in/api/login", {
         method: "POST",
         headers: {
-            "Content-type": "application/json"
+            "Content-type": "application/json",
+            'x-api-key': 'reqres-free-v1'
         },
-        body: JSON.stringify({ email, password})
+        body: JSON.stringify({ email, password })
     })
-    .then((data) =>{
-        alertType = 'success'
-        message = 'Inicio de sesión exitoso';
-        console.log('responde bien'+ data)
+    .then((response) =>{
+        if(response.status === 200){
+            alertType = 'success'
+            message = 'Inicio de sesión exitoso';
+            console.log('responde bien'+ response)
+            alertBuilder(alertType, message)
+            localStorage.setItem('token', 'sassedd4455544444')
+            setTimeout(() => {
+                location.href = 'admin/dashboard.html'
+            }, 2000) // 2000 ms = 2 segundos
+        }
+        else{
+            alertType = 'danger'
+            message = 'Correo o contraseña incorrectos.';
+            alertBuilder(alertType, message)
+        }        
     })
     .catch((error) => {
         alertType = 'danger'
-        message = 'Correo o contraseña incorrectos.';
+        message = 'Error inesperado.';
         console.error(error)
+        alertBuilder(alertType, message)
     })
+}
 
-    let alert = `
+function alertBuilder(alertType, message){
+    const alert = `
         <div class="alert alert-${alertType} alert-dismissible fade show" role="alert">
             ${message}
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
